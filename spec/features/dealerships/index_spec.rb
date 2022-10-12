7808cbe903180d25290330245e4084d73f0948bf
Eli Fuchsman <efuchsman@gmail.com>
2022-10-12 15:18:28 -0600

    Create show spec for dealerships

--- spec/features/dealerships/index_spec.rb
+++ spec/features/dealerships/index_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe "Dealerships Index Page" do
         dealership_1 = Dealership.create!(city: "Denver", dealername: "Eli's Used Car Palace", number_of_stars_rating: 3, lease_program: true)
         dealership_2 = Dealership.create!(city: "Aurora", dealername: "Shirley's Premier Used Cars", number_of_stars_rating: 5, lease_program: true)
         visit "/dealerships"
-        save_and_open_page
+        # save_and_open_page
         expect(page).to have_content(dealership_1.dealername)
         expect(page).to have_content(dealership_2.dealername)
       end
