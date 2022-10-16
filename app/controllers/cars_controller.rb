d25634ff2699904942ec88935301629e4e687837
Eli Fuchsman <efuchsman@gmail.com>
2022-10-16 17:39:37 -0600

    Create Edit and Update vehicle

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -4,17 +4,21 @@ class CarsController < ApplicationController
   end
 
   def edit
+    @car = Car.find(params[:id])
   end
 
   def update
+    car = Car.find(params[:id])
+    car.update(car_params)
+    redirect_to "/cars/#{car.id}"
   end
-  #  def new
-  #  end
-
-  #  def create
-  #  end
 
    def show
     @car = Car.find(params[:id])
    end
+
+   private
+   def car_params
+    params.permit(:make, :model, :year, :miles, :available_for_lease, :dealer_id, :price)
+    end
 end
--- spec/features/cars/edit_spec.rb
+++ spec/features/cars/edit_spec.rb
@@ -14,5 +14,69 @@
 require 'rails_helper'
 
 RSpec.describe "Car Update" do
+  describe "As a visitor" do
+    describe "When I visit a Car Show page" do
+      it "Then I see a link to update that car 'Update Vehicle'" do
+        dealership = Dealership.create!(city: "Denver", dealername: "Eli's Used Car Palace", number_of_stars_rating: 3, lease_program: true)
+        car_1 = Car.create!(make: "Audi", model: "A4", year: 2020, miles: 16000, available_for_lease: true, dealer_id: 1, price: 37000, dealership: dealership)
 
+        visit "/cars/#{car_1.id}"
+        expect(page).to have_link("Edit Vehicle")
+      end
+      describe "When I click the link" do
+        it "I am taken '/cars/id/edit'" do
+          dealership = Dealership.create!(city: "Denver", dealername: "Eli's Used Car Palace", number_of_stars_rating: 3, lease_program: true)
+          car_1 = Car.create!(make: "Audi", model: "A4", year: 2020, miles: 16000, available_for_lease: true, dealer_id: 1, price: 37000, dealership: dealership)
+          visit "/cars/#{car_1.id}"
+
+          click_link "Edit Vehicle"
+
+          expect(current_path).to eq("/cars/#{car_1.id}/edit")
+          expect(page).to have_content("Make")
+          expect(page).to have_content("Model")
+          expect(page).to have_content("Year")
+          expect(page).to have_content("Miles")
+          expect(page).to have_content("Available for lease?(true/false)")
+          expect(page).to have_content("Dealer id(serves no purpose. put any integer)")
+          expect(page).to have_content("Price")
+          expect(page).to have_button("Update Vehicle")
+        end
+      end
+
+      describe "When I fill out the form and click 'Update Vehicle'" do
+        it "the vehicle attributes are updated and I am redirected to the cars index" do
+          dealership = Dealership.create!(city: "Denver", dealername: "Eli's Used Car Palace", number_of_stars_rating: 3, lease_program: true)
+          car_1 = Car.create!(make: "Subaroo", model: "WRX", year: 2030, miles: 16000, available_for_lease: true, dealer_id: 1, price: 7000, dealership: dealership)
+          visit "/cars/#{car_1.id}"
+
+          expect(page).to have_content("Subaroo")
+          expect(page).to have_content("WRX")
+          expect(page).to have_content(2030)
+          expect(page).to have_content(7000)
+
+          click_link "Edit Vehicle"
+
+          expect(current_path).to eq("/cars/#{car_1.id}/edit")
+
+          fill_in(:make, with: "Subaru")
+          fill_in(:model, with: "WRX")
+          fill_in(:year, with: 2020)
+          fill_in(:miles, with: 22000)
+          fill_in(:available_for_lease, with: true)
+          fill_in(:dealer_id, with: 1)
+          fill_in(:price, with: 26000)
+
+          click_button "Update Vehicle"
+
+          expect(current_path).to eq("/cars/#{car_1.id}")
+          expect(page).to_not have_content("Subaroo")
+
+          click_link "Cars Index"
+
+          expect(current_path).to eq("/cars")
+          expect(page).to_not have_content("Subaroo")
+        end
+      end
+    end
+  end
 end
