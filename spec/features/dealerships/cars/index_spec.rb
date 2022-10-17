894ee200ed401cf33bd6a0ff7419dd9f76a9572b
Eli Fuchsman <efuchsman@gmail.com>
2022-10-17 14:27:04 -0600

    WIP user story 21

--- spec/features/dealerships/cars/index_spec.rb
+++ spec/features/dealerships/cars/index_spec.rb
@@ -62,4 +62,20 @@ RSpec.describe "Dealership Cars Index", type: :feature do
     expect("Audi").to appear_before("BMW")
     expect("BMW").to appear_before("Mercedes-Benz")
   end
+
+  describe "Display Records Over a Given Threshold " do
+    before :each do
+      @dealership = Dealership.create!(city: "Denver", dealername: "Eli's Used Car Palace", number_of_stars_rating: 3, lease_program: true)
+      @car_1 = @dealership.cars.create!(make: "Mercedes-Benz", model: "C300", year: 2019, miles: 24000, available_for_lease: true, dealer_id: 1, price: 28000)
+      @car_2 = @dealership.cars.create!(make: "BMW", model: "440i", year: 2017, miles: 51000, available_for_lease: false, dealer_id: 1, price: 19500)
+      @car_3 = @dealership.cars.create!(make: "Audi", model: "A4", year: 2020, miles: 16000, available_for_lease: true, dealer_id: 1, price: 37000)
+    end
+
+    it 'has button that allows the user to input a number value' do
+      visit "/dealerships/#{@dealership.id}/cars"
+
+      expect(page).to have_content("Display cars over a given mile threshold")
+      expect(page).to have_button("Show")
+    end
+  end
 end
