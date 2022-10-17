c7d6669a2016470b9860ca300c133ec69ff1458a
Eli Fuchsman <efuchsman@gmail.com>
2022-10-17 10:58:19 -0600

    WIP child delete

---
+++ spec/features/cars/destroy_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Destroy" do
+  describe "As a visitor" do
+    describe "When I visit a car show page" do
+      it "Has a button to delete the car" do
+
+      end
+    end
+  end
+end
